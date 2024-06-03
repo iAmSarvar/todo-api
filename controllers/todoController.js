d4ed30f89be9d5f18fa98bb14c5d746d3b56a383
iAmSarvar <iamsarvarsultanov@gmail.com>
2024-06-03 14:16:32 +0500

    finished CRUD controllers for todos

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -10,13 +10,88 @@ const getAllTodos = async (req, res) => {
       },
     });
   } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: "No todos found!",
+    });
+  }
+};
+
+const getTodo = async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        todo,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+const createTodo = async (req, res) => {
+  try {
+    const newTodo = await Todo.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        todo: newTodo,
+      },
+    });
+  } catch (error) {
     res.status(400).json({
       status: "fail",
-      message: "There is no todos!",
+      message: error.message,
+    });
+  }
+};
+
+const updateTodo = async (req, res) => {
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        todo: updatedTodo,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+const deleteTodo = async (req, res) => {
+  try {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
     });
   }
 };
 
 module.exports = {
   getAllTodos,
+  createTodo,
+  deleteTodo,
+  getTodo,
+  updateTodo,
 };
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -4,6 +4,15 @@ const todoController = require("../controllers/todoController");
 
 const router = express.Router();
 
-router.route("/").get(todoController.getAllTodos);
+router
+  .route("/")
+  .get(todoController.getAllTodos)
+  .post(todoController.createTodo);
+
+router
+  .route("/:id")
+  .get(todoController.getTodo)
+  .patch(todoController.updateTodo)
+  .delete(todoController.deleteTodo);
 
 module.exports = router;
