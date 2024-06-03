843430d5cbd230d2ef6d8e1d86cf49ee2879ee8c
iAmSarvar <iamsarvarsultanov@gmail.com>
2024-06-03 12:52:07 +0500

    initial setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const app = require("./app");
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
