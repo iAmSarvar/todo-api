73f3451bc03c58c8ca9d12255c8f54eb27abb541
iAmSarvar <iamsarvarsultanov@gmail.com>
2024-06-03 16:54:13 +0500

    404 page with error handling middleware

--- app.js
+++ app.js
@@ -7,4 +7,22 @@ app.use(express.json());
 
 app.use("/api/v1/todos", todoRouter);
 
+app.all("*", (req, res, next) => {
+  const error = new Error(`Page ${req.originalUrl} not found!`);
+  error.status = "fail";
+  error.statusCode = 404;
+
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  error.status = error.status || "error";
+  error.statusCode = error.statusCode || 500;
+
+  res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+  });
+});
+
 module.exports = app;
